b0a03a10ab6886a4adc65636e3d24983f9598a47
Diana Balaban <diana.rud.2012@mail.ru>
2022-07-23 23:17:49 +0300

    slider changed

--- js/swiper.js
+++ js/swiper.js
@@ -1,16 +1,67 @@
+ // gallary slider
 const swiper = new Swiper('.swiper', {
   slidesPerView: 3,
-  spaceBetween: 30,
+  spaceBetween: 50,
+  slidesPerGroup: 3,
   loop: true,
-  // пагинация
+  slidersPerColumnFill: 'row',
+  // pagination
   pagination: {
   el: '.swiper-pagination',
-  clickable: true
+  type: 'fraction',
   },
-  // навигация
+  // navigation
   navigation: {
   nextEl: '.swiper-button-next',
   prevEl: '.swiper-button-prev',
   },
+  breakpoints:{
+    768:{
+      spaceBetween: 38,
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+
+    1024:{
+      spaceBetween: 34,
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      },
+
+    1330:{
+      spaceBetween: 50,
+      slidesPerView: 3,
+      slidesPerColumn: 2,
+      slidesPerGroup: 3,
+    }
+  }
 });
 
+// projects swiper
+const projectsSwiper = new Swiper('.projects__swiper', {
+  slideClass: ('partners__swiper-slide'),
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  loop: true,
+  spaceBetween: 30,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints:{
+
+    668: {
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+      spaceBetween: 30,
+    },
+
+    1600:{
+      spaceBetween: 50,
+      slidesPerView: 3,
+      slidesPerGroup: 1,
+    }
+  }
+});
